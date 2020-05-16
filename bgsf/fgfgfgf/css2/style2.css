97fc0d6313ac15d6339e4869f27e2f16e00d65cf
andoriweb <61490810+andoriweb@users.noreply.github.com>
2020-05-16 07:40:34 +0300

    Delete style2.css

--- bgsf/fgfgfgf/css2/style2.css
+++
@@ -1,7 +0,0 @@
-@font-face {
-  font-family: 'Proxima Nova Reg';
-  src: url("../fonts/ProximaNovaRegular/ProximaNova-Regular.eot");
-  src: local("../fonts/ProximaNovaRegular/Proxima Nova Regular"), local("../fonts/ProximaNovaRegular/ProximaNova-Regular"), url("../fonts/ProximaNovaRegular/ProximaNova-Regular.eot?#iefix") format("embedded-opentype"), url("../fonts/ProximaNovaRegular/ProximaNova-Regular.woff2") format("woff2"), url("../fonts/ProximaNovaRegular/ProximaNova-Regular.woff") format("woff"), url("../fonts/ProximaNovaRegular/ProximaNova-Regular.ttf") format("truetype"), url("../fonts/ProximaNovaRegular/ProximaNova-Regular.svg#ProximaNova-Regular") format("svg");
-  font-weight: normal;
-  font-style: normal;
-}
